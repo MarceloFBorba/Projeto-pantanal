7fddb3c5fad201773d12efd45b8320ae304174bb
Marcelo Ferreira <84481185+MarceloFBorba@users.noreply.github.com>
2023-07-24 16:02:38 -0400

    excluindo alguns arquivos nao necessarios

--- code/index.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st 
-import pandas as pd
-
-st.title('Detecção de Fraude em Cartão de Crédito')
-
-st.subheader('Contexto')
-
-st.write('A detecção de fraudes tem como objetivo identificar atividades ou padrões não usuais. Por exemplo, para o setor financeiro: falsificação de assinaturas em cheques, clonagem de cartões de crédito, lavagem de dinheiro, declarar falência propositalmente (bankruptcy), etc.')
-
